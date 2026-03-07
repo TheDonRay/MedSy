0d48f5017ec7a6143f08f46f7fa186269b23c47c
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-06 21:38:03 -0500

    working on the layout of the backend and external service here

---
+++ backend/services/RxNormAPI.services.js
@@ -0,0 +1,7 @@
+//import the data that is passed from client said from what a client types in for symptoms get that data exported from previous route. 
+// build the RXNorm api so that the medicine can be called here organized then exported in another function. 
+const externServiceAPI = async () => { 
+
+} 
+
+module.exports = externServiceAPI; 
